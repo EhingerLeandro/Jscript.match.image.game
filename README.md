ab2847eabc67f430591cfba635c899f8b47a9468
EhingeLeandro <ehinger.leandro@hotmail.com>
2024-01-26 15:33:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+-This is a game where you need to find each twin pair of images, 
+so you can win.
+
+-Este es un juego en el que debes encontrar cada par gemelo de imágenes,
+para que puedas ganar.
---
+++ main.js
@@ -0,0 +1,149 @@
+const board = document.querySelector('.board'),
+      bigGame= document.querySelector('#bigGame'),
+      midGame= document.querySelector('#midGame'),
+      smallGame= document.querySelector('#smallGame'),
+      restart= document.querySelector('#restart');
+
+let unicodeEmot='&#x1F', 
+	rangeEmot= 25,
+	arrayEmot= [],
+	arrSelecEmot =[],
+	sizeGame=[16, 24, 32];
+
+for (let i=0; i<rangeEmot; ++i ){
+	arrayEmot.push(unicodeEmot+(400+i*2));
+}
+
+let arrEmotInitial=[];
+let arrRandomFin = [];
+let compareEmot = [];
+
+
+function iniRestart (){
+	board.innerHTML = `<div class='instruction'>
+					<div class='inner-inst'>
+						Chose the size of the Game
+					</div>
+				</div>`
+	arrRandomFin = [];
+	console.log(arrRandomFin);
+}
+
+iniRestart();
+
+function choseEmoticon(size, emotOptions){
+	let numEmotChosen= size/2;
+	let arrChosen =  emotOptions.slice(0, numEmotChosen);
+	let arrChosenB = arrChosen.map(item=> item+'b');
+	let arrEmotDupli = [...arrChosen, ...arrChosenB];
+	arrEmotInitial = arrEmotDupli;
+}
+
+const randomEmotOrder = (arr) =>{
+	let arrRandom=[],
+	arrApart = [...arr],
+	sizeArray = arr.length;
+	while ( sizeArray>0){
+		let currentEmot= Math.floor(Math.random() * sizeArray);
+		arrRandom.push({name:String(arrApart[currentEmot]).substring(0, 8), id: String(arrApart[currentEmot]).substring(5, 9), match: false});
+		arrApart.splice(currentEmot, 1);
+		sizeArray -= 1;
+	}
+	arrRandomFin = arrRandom;
+}
+
+const itMatches=()=>{
+	let compare = localStorage.getItem('comparative');
+	let arrCompare = JSON.parse(compare);
+
+	const card1 = document.querySelector('.card'+arrCompare[0]);
+	const card2 = document.querySelector('.card'+arrCompare[1]);
+
+	card1.classList.toggle('cardOpacity');
+	card2.classList.toggle('cardOpacity');
+
+	card1.disable=true;
+	card2.disable=false;
+
+}
+
+const dontMatch=()=>{
+
+	let compare = localStorage.getItem('comparative');
+	let arrCompare = JSON.parse(compare);
+
+
+	const card1 = document.querySelector('.card'+arrCompare[0]);
+	const spanCard1 = document.querySelector('.spanCard'+arrCompare[0]);
+	const card2 = document.querySelector('.card'+arrCompare[1]);
+	const spanCard2 = document.querySelector('.spanCard'+arrCompare[1]);
+	
+	card1.classList.toggle('cardToggle');
+	spanCard1.classList.toggle('spanToggle');
+	card2.classList.toggle('cardToggle');
+	spanCard2.classList.toggle('spanToggle');
+}
+
+
+const checkMatch =(pickEmot)=>{
+	compareEmot.push(pickEmot);
+	if(compareEmot.length >= 2){
+		localStorage.setItem('comparative', JSON.stringify(compareEmot));
+		if(compareEmot[0].substring(0, 3) === compareEmot[1].substring(0,3)){
+			setTimeout(itMatches, 500)
+			console.log(compareEmot);
+			compareEmot=[];
+		}else{
+			console.log(compareEmot)
+			setTimeout(dontMatch, 500)
+			compareEmot=[];
+		}
+	}else{
+
+	}
+	console.log(compareEmot);
+}
+
+const pickCardEmot = (pickEmot) =>{
+	console.log(pickEmot);
+	const card = document.querySelector('.card'+pickEmot);
+	const spanCard = document.querySelector('.spanCard'+pickEmot);
+	card.classList.toggle('cardToggle');
+	spanCard.classList.toggle('spanToggle');
+	checkMatch(pickEmot);
+}
+
+const renderCards = (size, renArr)=>{
+	for (let i=0; i<size; ++i){
+		let divCard = document.createElement('div');
+		divCard.classList.add('card');
+		divCard.classList.add('card'+renArr[i].id);
+		divCard.classList.add('cardToggle');
+		//Span is created in order to allow me to hide the emoticons
+		let spanCard = document.createElement('span');
+		spanCard.classList.add('spanCard'+renArr[i].id);
+		spanCard.classList.add('spanToggle');
+		spanCard.innerHTML = renArr[i].name;
+		divCard.append(spanCard);
+		divCard.addEventListener('click', ()=>pickCardEmot(renArr[i].id) )
+		board.append(divCard);
+	}
+}
+
+const chosenButton = (size) =>{
+	board.innerHTML='';
+	choseEmoticon(size, arrayEmot);
+	randomEmotOrder(arrEmotInitial);
+	renderCards(size, arrRandomFin);
+	console.log(arrRandomFin);
+}
+
+restart.addEventListener('click', iniRestart )
+
+bigGame.addEventListener('click', ()=>chosenButton(sizeGame[2]) )
+
+midGame.addEventListener('click', ()=>chosenButton(sizeGame[1]) )
+
+smallGame.addEventListener('click', ()=>chosenButton(sizeGame[0]) )
+
+
