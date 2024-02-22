4ba2f1289cb244622029abe03734ba6f9e837122
EhingeLeandro <ehinger.leandro@hotmail.com>
2024-02-22 12:24:18 -0500

    4° commit

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ let arrEmotInitial=[];
 let arrRandomFin = [];
 let compareEmot = [];
 
-
+//This part decides what is rendered inside the board, it depends on the language chosen previously.
 function iniRestart (language){
 	board.innerHTML = `<div  class='instruction'>
 					<div id='instruct' class='inner-inst'>
@@ -36,7 +36,7 @@ function iniRestart (language){
 }
 iniRestart(isEnglish);
 
-
+//Function that creates a array of emoticons.
 function choseEmoticon(size, emotOptions){
 	let numEmotChosen= size/2;
 	let arrChosen =  emotOptions.slice(0, numEmotChosen);
@@ -45,6 +45,7 @@ function choseEmoticon(size, emotOptions){
 	arrEmotInitial = arrEmotDupli;
 }
 
+//Function that generates another array with emoticons in a random order.
 const randomEmotOrder = (arr) =>{
 	let arrRandom=[],
 	arrApart = [...arr],
@@ -58,20 +59,24 @@ const randomEmotOrder = (arr) =>{
 	arrRandomFin = arrRandom;
 }
 
+//This functionlaity will be activated when a pair of cards matches.
 const itMatches=()=>{
+	//Here it gets the array from the localStorage 
 	let compare = localStorage.getItem('comparative');
 	let arrCompare = JSON.parse(compare);
 
 	const card1 = document.querySelector('.card'+arrCompare[0]);
 	const card2 = document.querySelector('.card'+arrCompare[1]);
 
+	//In this part, it toggles a class that allows to change the opacity of cards.
 	card1.classList.toggle('cardOpacity');
 	card2.classList.toggle('cardOpacity');
 
 	card1.disable=true;
-	card2.disable=false;
+	card2.disable=true;
 }
 
+//This functionlaity will be activated when a pair of cards DOESN'T match.
 const dontMatch=()=>{
 	let compare = localStorage.getItem('comparative');
 	let arrCompare = JSON.parse(compare);
@@ -88,10 +93,14 @@ const dontMatch=()=>{
 }
 
 
+/*With this function you use an array to save the record of those cards that
+have been selected (the first and second card), in order to compare them*/
 const checkMatch =(pickEmot)=>{
 	compareEmot.push(pickEmot);
 	if(compareEmot.length >= 2){
+		//The array is saved in localsotrage, so that it avoids scope problems.
 		localStorage.setItem('comparative', JSON.stringify(compareEmot));
+		//Here it compares the first and second card.
 		if(compareEmot[0].substring(0, 3) === compareEmot[1].substring(0,3)){
 			setTimeout(itMatches, 500)
 			console.log(compareEmot);
@@ -101,12 +110,11 @@ const checkMatch =(pickEmot)=>{
 			setTimeout(dontMatch, 500)
 			compareEmot=[];
 		}
-	}else{
-
 	}
 	console.log(compareEmot);
 }
 
+//This functionality is inside every card, and it's activated when one card is clicked.
 const pickCardEmot = (pickEmot) =>{
 	console.log(pickEmot);
 	const card = document.querySelector('.card'+pickEmot);
@@ -116,23 +124,28 @@ const pickCardEmot = (pickEmot) =>{
 	checkMatch(pickEmot);
 }
 
+//This function render all cards and gives to those cards.
 const renderCards = (size, renArr)=>{
+	//This for iterates through every card represented in the array.
 	for (let i=0; i<size; ++i){
 		let divCard = document.createElement('div');
 		divCard.classList.add('card');
 		divCard.classList.add('card'+renArr[i].id);
 		divCard.classList.add('cardToggle');
-		//Span is created in order to allow me to hide the emoticons
+		//Span is created in order to allow me to hide the emoticons.
 		let spanCard = document.createElement('span');
 		spanCard.classList.add('spanCard'+renArr[i].id);
 		spanCard.classList.add('spanToggle');
 		spanCard.innerHTML = renArr[i].name;
+		//Here span is render inside the div.
 		divCard.append(spanCard);
+		//In this part every card receives an eventListener with a function and an argument.
 		divCard.addEventListener('click', ()=>pickCardEmot(renArr[i].id) )
 		board.append(divCard);
 	}
 }
 
+//This function allows the app to change the size of the board.
 const chosenButton = (size) =>{
 	board.innerHTML='';
 	choseEmoticon(size, arrayEmot);
@@ -141,6 +154,7 @@ const chosenButton = (size) =>{
 	console.log(arrRandomFin);
 }
 
+//This function changes the information into spanish.
 const turnIntoSpanish =() =>{
 	isEnglish=false;
 	restart.innerText='Reiniciar';
@@ -151,6 +165,7 @@ const turnIntoSpanish =() =>{
 	iniRestart(isEnglish);
 }
 
+//This function changes the information into english.
 const turnIntoEnglish =() =>{
 	isEnglish=true;
 	restart.innerText='Restart';
